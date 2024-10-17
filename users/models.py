507a61a0ce1d3d83b303b506084ecc806177a65a
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-10-17 15:29:21 +0200

    Created user models and user manager

--- users/models.py
+++ users/models.py
@@ -1,14 +1,40 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext as _
 
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        first_name = extra_fields.get("first_name", "")
+        last_name = extra_fields.get("last_name", "")
+        user = self.model(email=email,first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+
 class User(AbstractUser):
     email = models.EmailField(_("Email address"), unique=True)
-    first_name = models.CharField(_("First name"), max_length=255)
-    last_name = models.CharField(_("Last name"), max_length=255)
-    password = models.CharField(_("Password"), max_length=255)
-
+    first_name = models.CharField(_("First name"), max_length=30, blank=True)
+    last_name = models.CharField(_("Last name"), max_length=30, blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
