437e57ae3603533ca999f3b56e7b7a7ecb80961f
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-10-17 14:38:00 +0200

    first

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(_("Email address"), unique=True)
+    first_name = models.CharField(_("First name"), max_length=255)
+    last_name = models.CharField(_("Last name"), max_length=255)
+    password = models.CharField(_("Password"), max_length=255)
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
