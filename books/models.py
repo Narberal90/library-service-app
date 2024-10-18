0c2bbabd86e8a4a1ffac686aa1c368b9ef286c08
Danyil <dartomov@gmail.com>
2024-10-18 07:58:07 +0200

    Added custom validator for book model; applied changes to db

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
-from django.core.validators import MinValueValidator
+from decimal import Decimal
+
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 
 class Book(models.Model):
@@ -10,8 +12,35 @@ class Book(models.Model):
     title = models.CharField(max_length=128, unique=True)
     authors = models.CharField(max_length=128, unique=True)
     cover = models.CharField(max_length=32, choices=CoverChoice, default=CoverChoice.HARD)
-    inventory = models.IntegerField(validators=[MinValueValidator(-1)])
-    daily_fee = models.DecimalField(decimal_places=2, max_digits=6, validators=[MinValueValidator(0)])
+    inventory = models.IntegerField()
+    daily_fee = models.DecimalField(decimal_places=2, max_digits=6)
 
     def __str__(self):
         return f"{self.title} ({self.authors}), available: {self.inventory}"
+
+    @staticmethod
+    def validate_non_negative_num(inventory: int, daily_fee: Decimal):
+        for num_attr_value, num_attr_name in [
+            (inventory, "inventory"),
+            (daily_fee, "daily_fee"),
+        ]:
+            if not 0 <= num_attr_value:
+                raise ValidationError(
+                    f"{num_attr_name} "
+                    "number value must be greater than equal 0. "
+                    f"Actual: {num_attr_value} < 0"
+                )
+
+    def clean(self):
+        Book.validate_non_negative_num(self.inventory, self.daily_fee)
+
+    def save(
+        self,
+        *args,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Book, self).save(force_insert, force_update, using, update_fields)
