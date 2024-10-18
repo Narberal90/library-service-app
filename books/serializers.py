984f43b54eddb93b983f9de0f05179b8719a2541
Danyil <dartomov@gmail.com>
2024-10-18 07:59:49 +0200

    Overrided validate method in BookSerializer

--- books/serializers.py
+++ books/serializers.py
@@ -7,3 +7,9 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ["id", "title", "authors", "cover", "inventory", "daily_fee"]
+
+    def validate(self, attrs):
+        Book.validate_non_negative_num(
+            inventory=attrs["inventory"], daily_fee=attrs["daily_fee"]
+        )
+        return attrs
