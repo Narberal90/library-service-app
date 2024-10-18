e770f9cadecd8ed61c106813ce115a6c0d6aa206
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-10-18 16:00:00 +0200

    added pagination to book

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from books.paginators import BookPagination
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -13,6 +14,7 @@ from books.serializers import (
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = BookPagination
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -24,7 +26,11 @@ class BookViewSet(viewsets.ModelViewSet):
 
         return BookSerializer
 
-    @action(methods=["POST"], detail=True, url_path="upload-image")
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image"
+    )
     def upload_image(self, request, pk=None) -> Response:
         book = self.get_object()
         serializer = self.get_serializer(book, data=request.data)
@@ -35,3 +41,4 @@ class BookViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
