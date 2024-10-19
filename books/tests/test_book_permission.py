b2046fc5264f34dfc071650c758d77378fadb12c
Halyna Rohatska <rohatskahr@gmail.com>
2024-10-19 15:47:54 +0300

    changed permission tests

--- books/tests/test_book_permission.py
+++ books/tests/test_book_permission.py
@@ -12,7 +12,7 @@ BOOK_URL = reverse("book:book-list")
 class TestBookAdmin(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user = get_user_model().objects.create_user(
+        self.user = get_user_model().objects.create_superuser(
             email="TonyStark@gmail.com", password="TonyStarkHasTheHeart", is_staff=True
         )
         self.client.force_authenticate(self.user)
@@ -26,6 +26,20 @@ class TestBookAdmin(TestCase):
         }
         self.book = Book.objects.create(**self.book_payload)
 
+    def test_admin_create_book_success(self):
+        create_payload = {
+            "title": "The Great",
+            "authors": "Scott",
+            "cover": "Soft",
+            "inventory": 7,
+            "daily_fee": "23.00"
+        }
+        url = reverse("book:book-detail", args=[self.book.id])
+        res = self.client.put(url, create_payload)
+
+        self.book.refresh_from_db()
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
     def test_admin_update_book_success(self):
         update_payload = {
             "title": "The Great Gatsby (Updated)",
@@ -34,7 +48,7 @@ class TestBookAdmin(TestCase):
             "inventory": 10,
             "daily_fee": "3.00"
         }
-        url = reverse("book:book-detail", args=[self.book.id])
+        url = reverse("book:book-create", args=[self.book.id])
         res = self.client.put(url, update_payload)
 
         self.book.refresh_from_db()
@@ -48,23 +62,3 @@ class TestBookAdmin(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Book.objects.filter(id=self.book.id).exists())
-
-class TestBookNonAdmin(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.non_admin_user = get_user_model().objects.create_user(
-            email="test@gmail.com", password="test"
-        )
-        self.client.force_authenticate(self.non_admin_user)
-
-        self.payload = {
-            "title": "test",
-            "authors": "test",
-            "cover": "Hard",
-            "inventory": 5,
-            "daily_fee": "2.50"
-        }
-
-    def test_non_admin_user_create_book_forbidden(self):
-        res = self.client.post(BOOK_URL, self.payload)
-        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
