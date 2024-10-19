8a1473c083bb42c30a78911e4745b1ba982ed8b1
Halyna Rohatska <rohatskahr@gmail.com>
2024-10-19 15:47:54 +0300

    changed permission tests

--- backend/books/tests/test_book_permission.py
+++ backend/books/tests/test_book_permission.py
@@ -12,7 +12,7 @@ BOOK_URL = reverse("book:book-list")
 class TestBookAdmin(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user = get_user_model().objects.create_user(
+        self.user = get_user_model().objects.create_superuser(
             email="TonyStark@gmail.com", password="TonyStarkHasTheHeart", is_staff=True
         )
         self.client.force_authenticate(self.user)
@@ -48,7 +48,7 @@ class TestBookAdmin(TestCase):
             "inventory": 10,
             "daily_fee": "3.00"
         }
-        url = reverse("book:book-detail", args=[self.book.id])
+        url = reverse("book:book-create", args=[self.book.id])
         res = self.client.put(url, update_payload)
 
         self.book.refresh_from_db()
@@ -62,33 +62,3 @@ class TestBookAdmin(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Book.objects.filter(id=self.book.id).exists())
-
-    def test_create_book_missing_fields(self):
-        create_payload = {
-            "authors": "Scott",
-            "cover": "Soft",
-        }
-        url = reverse("book:book-list")
-        res = self.client.post(url, create_payload)
-
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_update_book_invalid_inventory(self):
-        update_payload = {
-            "title": "Invalid Book",
-            "authors": "Author",
-            "cover": "Soft",
-            "inventory": -1,
-            "daily_fee": "3.00"
-        }
-        url = reverse("book:book-detail", args=[self.book.id])
-        res = self.client.put(url, update_payload)
-
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_get_book_success(self):
-        url = reverse("book:book-detail", args=[self.book.id])
-        res = self.client.get(url)
-
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data['title'], self.book.title)
