cd589e307b6a034283bd87d94452c0f0fa38c5fb
Halyna Rohatska <rohatskahr@gmail.com>
2024-10-20 17:07:05 +0300

    added tests for borrowings

--- borrowings/tests/test_borrowings.py
+++ borrowings/tests/test_borrowings.py
@@ -1,6 +1,7 @@
-from datetime import timedelta, date
+from datetime import timedelta
 
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
 from django.test import TestCase
 from django.urls import reverse
 from django.core.exceptions import ValidationError
@@ -11,10 +12,10 @@ from rest_framework import status
 from books.models import Book
 from borrowings.models import Borrowing
 from borrowings.serializers import (
-    BorrowingSerializer,
     BorrowingListSerializer,
     BorrowingDetailSerializer,
 )
+from borrowings.signals import send_borrowing_notification
 
 BORROWING_URL = reverse("borrowing:borrowing-list")
 
@@ -50,12 +51,16 @@ class UnauthenticatedBorrowingApiTests(TestCase):
         self.client = APIClient()
 
     def test_auth_required_for_borrowing_list(self):
+        """The test verifies that the user must be authorized
+         to receive the list of borrowings"""
+
         res = self.client.get(BORROWING_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class AuthenticatedBorrowingApiTests(TestCase):
     def setUp(self):
+        post_save.disconnect(send_borrowing_notification, sender=Borrowing)
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
             email="test@email.test",
@@ -85,9 +90,32 @@ class AuthenticatedBorrowingApiTests(TestCase):
             expected_return_date="2025-01-24"
         )
 
+    def test_admin_borrowing_list(self):
+        """Test successful access to the list
+        of all borrowings for admin"""
+
+        sample_borrowing()
+        client = APIClient()
+        user = get_user_model().objects.create_superuser(
+            email="super@admin.com",
+            password="greatadmin"
+        )
+        client.force_authenticate(user)
+        res = client.get(BORROWING_URL)
+
+        borrowings = Borrowing.objects.all()
+        serializer = BorrowingListSerializer(borrowings, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            list(res.data["results"]),
+            list(serializer.data)
+        )
+
     def test_borrowing_list(self):
         """Test successful access to the list
-        of borrowings for an authorized use"""
+        of borrowings for an authorized user"""
+
         borrowing = sample_borrowing()
         res = self.client.get(BORROWING_URL)
         borrowings = Borrowing.objects.filter(user=self.user)
@@ -101,6 +129,9 @@ class AuthenticatedBorrowingApiTests(TestCase):
         )
 
     def test_filter_borrowing_list_by_book_title(self):
+        """The test checks whether the list of borrowing
+         can be correctly filtered by book title"""
+
         res = self.client.get(BORROWING_URL, {"book_title": "book 1"})
         serializer = BorrowingListSerializer(self.borrowing_1)
 
@@ -108,6 +139,9 @@ class AuthenticatedBorrowingApiTests(TestCase):
         self.assertEqual(res.data["results"][0], serializer.data)
 
     def test_filter_borrowing_list_by_active_status_true(self):
+        """The test checks whether the list of borrowings can
+         be correctly filtered by active status equal true"""
+
         res = self.client.get(BORROWING_URL, {"is_active": True})
 
         borrowings = Borrowing.objects.filter(actual_return_date__isnull=True)
@@ -117,7 +151,9 @@ class AuthenticatedBorrowingApiTests(TestCase):
         self.assertEqual(res.data["results"], serializer.data)
 
     def test_filter_borrowing_list_by_active_status_false(self):
-        borrowing = sample_borrowing(user=self.user)
+        """The test checks whether the list of borrowings can
+         be correctly filtered by active status equal false"""
+
         res = self.client.get(BORROWING_URL, {"is_active": False})
 
         borrowings = Borrowing.objects.filter(actual_return_date__isnull=False)
@@ -127,6 +163,9 @@ class AuthenticatedBorrowingApiTests(TestCase):
         self.assertEqual(res.data["results"], serializer.data)
 
     def test_retrieve_borrowing_detail(self):
+        """The test checks the possibility of obtaining detailed
+         information about a specific loan."""
+
         res = self.client.get(detail_url(self.borrowing_1.id))
         serializer = BorrowingDetailSerializer(self.borrowing_1)
 
@@ -134,6 +173,9 @@ class AuthenticatedBorrowingApiTests(TestCase):
         self.assertEqual(res.data, serializer.data)
 
     def test_expected_return_date_before_borrow_date(self):
+        """The test verifies that the expected return date cannot
+         be earlier than the loan date."""
+
         self.borrowing_1.expected_return_date = self.borrowing_1.borrow_date - timedelta(days=1)
 
         with self.assertRaises(ValidationError) as context:
@@ -145,6 +187,9 @@ class AuthenticatedBorrowingApiTests(TestCase):
         )
 
     def test_actual_return_date_before_borrow_date(self):
+        """The test verifies that the actual return date cannot
+         be earlier than the loan date."""
+
         self.borrowing_2.actual_return_date = self.borrowing_2.borrow_date - timedelta(days=1)
 
         with self.assertRaises(ValidationError) as context:
@@ -153,38 +198,9 @@ class AuthenticatedBorrowingApiTests(TestCase):
         self.assertIn("The actual return date cannot be before the borrow date.", str(context.exception))
 
     def test_valid_return_dates(self):
+        """The test verifies that the actual return date is set correctly"""
         self.borrowing_2.actual_return_date = self.borrowing_2.borrow_date + timedelta(days=5)
         try:
             self.borrowing_2.full_clean()
         except ValidationError:
             self.fail("ValidationError raised unexpectedly!")
-
-
-class BorrowingSerializerTests(TestCase):
-    def setUp(self):
-        self.book = Book.objects.create(
-            title="serializer test",
-            authors="serializer",
-            cover="Hard",
-            inventory=5,
-            daily_fee="3.00"
-        )
-        self.user = get_user_model().objects.create_user(
-            email="serializer@email.com",
-            password="PassworD"
-        )
-        self.valid_data = {
-            "book": self.book,
-            "borrow_date": date.today(),
-            "expected_return_date": date.today() + timedelta(days=10),
-            "user": self.user
-        }
-        self.serializer = BorrowingSerializer()
-
-    def test_create_borrowing_with_available_inventory(self):
-        """The test verifies that a book can be borrowed if copies are available"""
-        borrowing = self.serializer.create(self.valid_data)
-        self.book.refresh_from_db()
-
-        self.assertEqual(self.book.inventory, 4)
-        self.assertIsInstance(borrowing, Borrowing)
