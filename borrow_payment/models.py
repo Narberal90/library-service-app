090b859ddf315461b94a85ed82175fe1456ee61e
Danyil <dartomov@gmail.com>
2024-10-20 12:28:35 +0200

    Changed borrowing field relation in Payment model

--- borrow_payment/models.py
+++ borrow_payment/models.py
@@ -18,7 +18,7 @@ class Payment(models.Model):
     type = models.CharField(
         max_length=32, choices=TypeChoice, default=TypeChoice.PAYMENT
     )
-    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+    borrowing = models.OneToOneField(Borrowing, on_delete=models.CASCADE, related_name="payment")
     session_url = models.URLField()
     session_id = models.CharField(max_length=124)
     money_to_pay = models.DecimalField(decimal_places=2, max_digits=6)
