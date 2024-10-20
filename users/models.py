49233461e758c52f1e2457cb58420dbb17330d20
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-10-20 10:50:35 +0300

    implemented additional attr for user for connections with tg

--- users/models.py
+++ users/models.py
@@ -8,6 +8,7 @@ from users.managers.user_manager import UserManager
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("Email address"), unique=True)
+    telegram_id = models.BigIntegerField(null=True, blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
+from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 from rest_framework.settings import api_settings
+from rest_framework.views import APIView
 
 from users.serializers import UserSerializer, AuthTokenSerializer
+from .models import User
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -22,3 +27,25 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class UpdateTelegramIDView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        email = request.data.get("email")
+        telegram_id = request.data.get("telegram_id")
+
+        try:
+            user = User.objects.get(email=email)
+            user.telegram_id = telegram_id
+            user.save()
+            return Response(
+                {"message": "Telegram ID updated!"},
+                status=status.HTTP_200_OK
+            )
+        except User.DoesNotExist:
+            return Response(
+                {"error": "No user with this email address was found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
