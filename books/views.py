f5ca10329f847f95217a90ca66004cfe77f192f2
Halyna Rohatska <rohatskahr@gmail.com>
2024-10-20 19:12:04 +0300

    removed blank line

--- books/views.py
+++ books/views.py
@@ -41,4 +41,3 @@ class BookViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
