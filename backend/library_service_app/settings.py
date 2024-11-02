4fa7cde89d17088483dfc06669ff972801b58b0b
Danyil <dartomov@gmail.com>
2024-11-02 10:50:10 +0100

    Moved permissions to project folder.

--- backend/library_service_app/settings.py
+++ backend/library_service_app/settings.py
@@ -145,12 +145,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    # "DEFAULT_AUTHENTICATION_CLASSES": [
-    #     "rest_framework_simplejwt.authentication.JWTAuthentication",
-    # ],
-    # "DEFAULT_PERMISSION_CLASSES": [
-    #     "books.permissions.IsAdminOrIfAuthenticatedReadOnly",
-    # ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "library_service.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ],
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle"
