5f27355d1b7e180b719824d216e9aeedcf914974
Danyil <dartomov@gmail.com>
2024-11-02 10:23:26 +0100

    Added ViewSet for Payment model.

--- backend/borrow_payment/views.py
+++ backend/borrow_payment/views.py
@@ -2,6 +2,10 @@ from django.http import HttpRequest
 from django.shortcuts import redirect
 from django.urls import reverse
 
+from rest_framework import viewsets
+
+from borrow_payment.models import Payment
+from borrow_payment.serializers import PaymentListSerializer, PaymentSerializer
 from borrowings.models import Borrowing
 
 
@@ -18,3 +22,23 @@ def success_session(request: HttpRequest):
         return redirect(url)
 
     return redirect(reverse("borrowings:borrowing-list"))
+
+
+class PaymentViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Payment.objects.all().select_related()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PaymentListSerializer
+        return PaymentSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.request.user.is_staff:
+            show_all = self.request.query_params.get("show_all", "None")
+
+            if show_all == "true":
+                return queryset
+
+        return queryset.filter(borrowing__user=self.request.user)
