75a838a0b8f28a60879124668158de191189f5c1
Danyil <dartomov@gmail.com>
2024-11-02 10:20:03 +0100

    Included in project urls borrowings and borrow_payment urls.

--- backend/library_service_app/urls.py
+++ backend/library_service_app/urls.py
@@ -37,4 +37,6 @@ urlpatterns = [
     ),
     path("api/users/", include("users.urls", namespace="users")),
     path("api/library/", include("books.urls", namespace="book")),
+    path("api/borrowings/", include("borrowings.urls", namespace="borrowing")),
+    path("api/borrow-payments/", include("borrow_payment.urls", namespace="borrow_payment")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
