6ddbb55699d7e863152b62f1c03822f186034546
Danyil <dartomov@gmail.com>
2024-11-08 16:13:54 +0100

    Fixed telegram import and docker-compose app build

--- telegram/main.py
+++ telegram/main.py
@@ -8,6 +8,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 from telegram.bot import bot
+import telegram.handlers
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -24,6 +26,7 @@ def start_bot_polling():
 
 @app.on_event("startup")
 async def startup_event():
+    print("startup_event")
     threading.Thread(target=start_bot_polling, daemon=True).start()
 
 
